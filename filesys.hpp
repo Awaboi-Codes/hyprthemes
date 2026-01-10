09868377e3ed725ae5897de0cdd1beece19ec1ad
Awaboi-Codes <kiw1boxedu@gmail.com>
2026-01-10 13:01:34 -0500

    added setup.sh (does nothing right now)

--- filesys.hpp
+++ filesys.hpp
@@ -2,15 +2,17 @@
 #define HYPRTHEMES_FILESYS_HPP
 
 #include <filesystem>
+#include <iostream>
+#include <fstream>
 #include <string>
+#include <sstream>
+#include <cstdlib>
 
 namespace fs = std::filesystem;
 
 using str = std::string;
 using ostream = std::ostringstream;
 using fstream = std::ifstream;
-
-using str = std::string;
 
 str getFileContents(const str& filename);
 
